6d8b9e3481f5bf54784ebc2ab87d8350f21bb146
Divyansh <divyanshm1609@gmail.com>
2021-06-14 13:02:47 +0530

    Configure mongodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,10 @@
+
+const DB_user='baahubali7781';
+const password=encodeURIComponent('Hello@12345');
+const DB_URL=`mongodb+srv://${DB_user}:${password}@cluster0.3hoof.mongodb.net/todo-list?retryWrites=true&w=majority`
+
 const mongoose=require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo_list');
+mongoose.connect(DB_URL || 'mongodb://localhost/todo_list');
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error while creating database'));
 db.once('open',function(){
